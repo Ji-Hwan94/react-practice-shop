59842c6a9d80865cf5de582a38e47367ee85cd5a
황지환 <ghkdrbqhd2@naver.com>
2023-12-14 18:12:52 +0900

    react 연습 useEffect

--- src/ui/detail/detail.js
+++ src/ui/detail/detail.js
@@ -1,4 +1,8 @@
-import { useParams } from "react-router-dom"
+import { useEffect, useState } from "react";
+import { useParams, useNavigate } from "react-router-dom"
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
+
 
 export default function Detail({shoes}){
 
@@ -6,23 +10,54 @@ export default function Detail({shoes}){
     let detailShoes = shoes.find((param) => {
         return param.id == id;
     })
+    let navigate = useNavigate();
+    let [display, setDisplay] = useState(true);
+    let [numChk, setNumChk] = useState(detailShoes.price);
+
+    useEffect(() => {
+        setTimeout(()=> {
+            setDisplay(false);
+        }, 2000);
+    }, []);
+
+    useEffect(() => {
+        let numericPattern = /^\d+$/;
+        if(!numericPattern.test(numChk)){
+            alert("그러지 말아 주세요");
+        }
+    }, [numChk])
+
     return(
         <>
             {
                 detailShoes !== undefined ? 
-                <div className="container">
-                    <div className="row">
-                        <div className="col-md-6">
-                            <img src={"https://codingapple1.github.io/shop/shoes"+id+".jpg"} width="100%" />
-                        </div>
-                        <div className="col-md-6">
-                            <h4 className="pt-5">{detailShoes.title}</h4>
-                            <p>{detailShoes.content}</p>
-                            <p>{detailShoes.price}</p>
-                            <button className="btn btn-danger">주문하기</button> 
+                    <div className="container">
+                        {
+                            display ? 
+                                <div className="alert alert-danger">
+                                    2초이내 사라지렴
+                                </div>
+                                : null
+                        }
+                        
+                        <div className="row">
+                            <div className="col-md-6">
+                                <img src={"https://codingapple1.github.io/shop/shoes"+id+".jpg"} width="100%" />
+                            </div>
+                            <div className="col-md-6">
+                                <h4 className="pt-5">{detailShoes.title}</h4>
+                                <p>{detailShoes.content}</p>
+                                <InputGroup className="sm-6">
+                                    <Form.Control defaultValue={detailShoes.price} onChange={(e) => {
+                                        setNumChk(e.target.value);
+                                    }}/>
+                                </InputGroup>
+                                <p>{detailShoes.price}</p>
+                                <button className="btn btn-danger">주문하기</button> 
+                                <button className="btn" onClick={() => {navigate(-1)}}>뒤로가기</button> 
+                            </div>
                         </div>
                     </div>
-                </div> 
                 : 
                 <div className="container">
                     없는 건디요
--- src/ui/home/card.js
+++ src/ui/home/card.js
@@ -1,9 +1,11 @@
 import Col from 'react-bootstrap/Col';
+import { useNavigate } from 'react-router-dom';
 
 export default function Card({obj, i}){
+  let navigate = useNavigate();
     return (
       <Col key={i}>
-        <img src={"https://codingapple1.github.io/shop/shoes"+(i+1)+".jpg"} className='list-img'/>
+        <img src={"https://codingapple1.github.io/shop/shoes"+(i+1)+".jpg"} className='list-img' onClick={() => { navigate("/detail/"+(i+1)) }}/>
         <h4>{obj.title}</h4>
         <p>{obj.price}</p> 
       </Col>
