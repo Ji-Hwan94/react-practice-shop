9b2e757709809a2b2aa18a4f627d82ab58fdbc9a
황지환 <ghkdrbqhd2@naver.com>
2023-12-21 15:17:27 +0900

    react 마지막

--- src/App.js
+++ src/App.js
@@ -3,13 +3,20 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
-import { Route, Routes, Link, useNavigate, Outlet } from 'react-router-dom'
+import { Route, Routes, useNavigate } from 'react-router-dom'
 import Home from './ui/home/home'
-import Detail from './ui/detail/detail';
-import Event from './ui/event/event';
-import Cart from './ui/cart/cart'
-import { createContext, useEffect, useState } from 'react';
+import { createContext, lazy, useEffect, useState, Suspense } from 'react';
 import data from './data';
+import axios from 'axios';
+import { useQuery } from 'react-query';
+
+// 아래의 컴포넌트는 바로 랜더링 될 이유가 없기 때문에, lazy 함수를 사용해서 성능을 개선 시킨다. => Suspense를 사용해서 lazy 컴포넌트가 로딩 될때 대체 할만한 컴포넌트를 생성한다.
+// import Detail from './ui/detail/detail';
+// import Event from './ui/event/event';
+// import Cart from './ui/cart/cart'
+const Detail = lazy(() => import('./ui/detail/detail'));
+const Event = lazy(() => import('./ui/event/event'));
+const Cart = lazy(() => import('./ui/cart/cart'));
 
 // context api
 // 안쓰는 이유 
@@ -22,6 +29,15 @@ function App() {
   let navigate = useNavigate();
   let [shoes] = useState(data);
   let [재고] = useState([10, 11, 12]);
+
+  // 실시간 데이터를 가져옴 useQuery
+  let result = useQuery("getUser", () => 
+    axios.get("https://codingapple1.github.io/userdata.json")
+    .then((a)=> {
+      return a.data;
+    }), {staleTime : 2000} // 2초 이내로 화면에 다시 들어가면 axios 안탐
+  )
+
   useEffect(() => {
     localStorage.setItem("watched", JSON.stringify([]));
   }, [])
@@ -30,12 +46,17 @@ function App() {
     <div className="App">
       <Navbar bg="dark" data-bs-theme="dark">
           <Container>
-            <Navbar.Brand href="#home">ShoeShop</Navbar.Brand>
+            <Navbar.Brand href="/">ShoeShop</Navbar.Brand>
             <Nav className="me-auto">
                 <Nav.Link onClick={() => {navigate("/")}}>Home</Nav.Link>
                 <Nav.Link onClick={() => {navigate("/detail")}}>Detail</Nav.Link>
                 <Nav.Link onClick={() => {navigate("/event")}}>Event</Nav.Link>
                 <Nav.Link onClick={() => {navigate("/cart")}}>Cart</Nav.Link>
+            </Nav>
+            <Nav className='dataName'>
+              {
+                result.isLoading ? "로딩 중" : result.data.name
+              }
             </Nav>
           </Container>
       </Navbar>
@@ -43,16 +64,26 @@ function App() {
         <Route path='/' element={ <Home shoes={shoes}></Home> }/>
         <Route path='/detail/:id' 
           element={
-            <Context1.Provider value={{ 재고, shoes }}>
-              <Detail shoes={shoes}></Detail> 
-            </Context1.Provider>
+            <Suspense fallback={<>loading</>}>
+              <Context1.Provider value={{ 재고, shoes }}>
+                <Detail shoes={shoes}></Detail> 
+              </Context1.Provider>
+            </Suspense>
           }
         />
-        <Route path='/event' element={ <Event></Event> }>
+        <Route path='/event' element={ 
+          <Suspense fallback={<>loading</>}>
+            <Event></Event> 
+          </Suspense>
+        }>
           <Route path='one' element={<>첫 주문시 양배추즙 서비스</> } />
           <Route path='two' element={<>생일기념 쿠폰 받기</>} />
         </Route>
-        <Route path='/cart' element={<Cart></Cart>}>
+        <Route path='/cart' element={
+          <Suspense fallback={<>loading</>}>
+            <Cart></Cart>
+          </Suspense>
+        }>
 
         </Route>
         <Route path='*' element={ <>없는 페이지</> }/>
--- src/ui/cart/cart.js
+++ src/ui/cart/cart.js
@@ -2,6 +2,15 @@ import { Table } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import { changeAge } from "../../store/userSlice";
 import { increment, deleteCart } from "../../store";
+import { memo, useState } from "react";
+
+// 부모컴포넌트가 렌더링 되는 경우 자식 컴포넌트도 재렌더링이 되기 때문에
+// 꼭 필요한 경우에만 렌더링을 일으키는 함수 memo
+// props가 변경될 때 (기존props와 신규props를 비교한 후 렌더링함)
+let Child = memo( function(){
+    console.log("재랜더링됨");
+    return <>자식임</>
+})
 
 
 export default function Cart(){
@@ -13,8 +22,13 @@ export default function Cart(){
     // store에 요청을 보냄
     let dispatch = useDispatch();
 
+    let [count, setCount] = useState(0);
+
     return (
         <>
+            <Child count={count}></Child>
+            <button onClick={() => {setCount(count++)}}>+</button>
+
             <h6>{name.name}의 장바구니, 나이 {name.age}</h6>
             <button onClick={() => {
                 dispatch(changeAge(10));
